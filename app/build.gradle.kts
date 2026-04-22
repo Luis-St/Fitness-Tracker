9ed7761af55946546adac6282039ef8f9269c6ba
Luis <luis@staudt-fridingen.de>
2026-04-23 00:43:29 +0200

    updated app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
 		applicationId = "net.luis.tracker"
 		minSdk = 36
 		targetSdk = 36
-		versionCode = 4
-		versionName = "1.3.0"
+		versionCode = 5
+		versionName = "1.4.0"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
